88869f557e37c0aec60affe750522e7a8f96537a
Dyestrm <johnmark.panganiban@wvsu.edu.ph>
2026-03-29 23:17:18 +0800

    feat: Add Builders for Animal and Pet
    - Renames Pet constructors, assumes Pet requires a nickname

--- CS 2B/PANGANIBAN_JOHN_MARK/PANGANIBAN-UNIT_5_PART_1/animal.dart
+++ CS 2B/PANGANIBAN_JOHN_MARK/PANGANIBAN-UNIT_5_PART_1/animal.dart
@@ -15,3 +15,14 @@ class Animal {
     // outputs summary of Animal's information
   }
 }
+
+class AnimalBuilder {
+
+  List<Animal> buildFiveAnimals() => [
+    Animal('Shark', 'Animalia', '1999', 0),
+    Animal('Dog', 'Animalia', '1998', 4),
+    Animal('Cat', 'Animalia', '1997', 4),
+    Animal('Eagle', 'Animalia', '2001', 2),
+    Animal('Snake', 'Animalia', '2000', 0),
+  ];
+}
--- CS 2B/PANGANIBAN_JOHN_MARK/PANGANIBAN-UNIT_5_PART_1/pet.dart
+++ CS 2B/PANGANIBAN_JOHN_MARK/PANGANIBAN-UNIT_5_PART_1/pet.dart
@@ -2,8 +2,8 @@ class Pet {
   String nickname = "";
   int kindness = 0;
 
-  Pet();
-  Pet.withNickName(this.nickname) { this.kindness = 50; }
+  Pet(this.nickname) { this.kindness = 50; }
+  Pet.noNickName(); 
 
   void kick() {
     // decreases kindness amount 
@@ -17,4 +17,13 @@ class Pet {
   void feed() {
     // increases kindness amount based on food
   }
+}
+
+class PetBuilder {
+  
+  List<Pet> buildThreePets() => [
+    Pet('Nasch'),
+    Pet.noNickName(),
+    Pet('Galaxy')
+  ];
 }
